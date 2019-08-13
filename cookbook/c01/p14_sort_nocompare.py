62384b2b7fc04d18b369f199486ddf4045d6ed2a
Wallace <r2010shadow@163.com>
2019-08-13 15:05:46 +0800

    fix
    Add line 23

--- cookbook/c01/p14_sort_nocompare.py
+++ cookbook/c01/p14_sort_nocompare.py
@@ -19,8 +19,9 @@ def sort_notcompare():
     print(users)
     print(sorted(users, key=lambda u: u.user_id))
 
-    from operator import attrgetter
-    print(sorted(users, key=attrgetter('user_id')))
+from operator import attrgetter
+users= [User(23), User(3), User(99)]
+print(sorted(users, key=attrgetter('user_id')))
 
     # print(sorted(users, key=attrgetter('last_name', 'first_name')))
 
